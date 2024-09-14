f65f6b8e07d23f1f4d068165f789ce2b869e5cbb
joeltlc96 <joel.vinanscatecnologicoliceocristiano.edu.ec>
2024-09-13 21:45:32 -0500

    Add project files.

---
+++ ApiA/Controllers/PeliculasController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiA.Context;
+using ApiA.Models;
+using System.Text;
+using System.Text.Json;
+
+namespace ApiA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeliculasController : ControllerBase
+    {
+        private const string BaseUrlApiB = "https://localhost:7076/api/Pelicula";
+
+        private readonly BaseAContext _context;
+
+        public PeliculasController(BaseAContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Peliculas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Pelicula>>> GetPeliculas()
+        {
+            return await _context.Peliculas.ToListAsync();
+        }
+
+        // GET: api/Peliculas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Pelicula>> GetPelicula(int id)
+        {
+            var pelicula = await _context.Peliculas.FindAsync(id);
+
+            if (pelicula == null)
+            {
+                return NotFound();
+            }
+
+            return pelicula;
+        }
+
+        // PUT: api/Peliculas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPelicula(int id, Pelicula pelicula)
+        {
+            if (id != pelicula.IdPelicula)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(pelicula).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PeliculaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Peliculas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Pelicula>> PostPelicula(Pelicula pelicula)
+        {
+            _context.Peliculas.Add(pelicula);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPelicula", new { id = pelicula.IdPelicula }, pelicula);
+
+            using (HttpClient client = new HttpClient())
+            {
+                string contenidoJson = JsonSerializer.Serialize(pelicula);
+                var contenidoPeticion = new StringContent(contenidoJson, Encoding.UTF8, "application/json");
+
+                try
+                {
+                    HttpResponseMessage respuestaHttp = await client.PostAsync(BaseUrlApiB, contenidoPeticion);
+
+                    if (respuestaHttp.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("Servicio creado exitosamente en BASE_A.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error al crear el servicio en BASE_A: {respuestaHttp.StatusCode}");
+                    }
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine($"Excepción al intentar crear el servicio en BASE_A: {e.Message}");
+                }
+            }
+        }
+
+        // DELETE: api/Peliculas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePelicula(int id)
+        {
+            var pelicula = await _context.Peliculas.FindAsync(id);
+            if (pelicula == null)
+            {
+                return NotFound();
+            }
+
+            _context.Peliculas.Remove(pelicula);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PeliculaExists(int id)
+        {
+            return _context.Peliculas.Any(e => e.IdPelicula == id);
+        }
+    }
+}
